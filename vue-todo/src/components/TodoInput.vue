b0172f531703adc5774248e6861b1cd6e0726419
hyr <hyr2902@gmail.com>
2019-06-20 04:27:07 +0900

    등록, 목록 기능 구현

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,15 +1,61 @@
 <template>
-    <div>
-        input
+    <div class="inputBox shadow">
+        <!-- v-model 은 vueJS로 data를 동적으로 매핑하기 위한 것
+             이 input에 값을 입력하면 vueJS의 newTodoItem 이라는 data 변수에 값이 반영된다.
+             onclick으로 keyup이벤트가 enter키에서 발생했을때 메소드를 실행한다. -->
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <!-- v-on:click="메소드명" 은 onclick="메소드명" 과 같다 -->
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
     </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods: {
+        addTodo: function(){
+            // addTodo 메소드 실행시 this(이 뷰 객체)의 newTodoItem 변수를 콘솔에 찍음
+            console.log(this.newTodoItem);
+            localStorage.setItem(this.newTodoItem, this.newTodoItem);
+            this.clearInput();
+        },
+        clearInput: function(){
+            // input창 값 초기화
+            this.newTodoItem = '';
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus{
+    outline: none;
+}
+.inputBox{
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input{
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer{
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn{
+    color: white;
+    vertical-align: middle;
+}
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,15 +1,78 @@
 <template>
     <div>
-        List
+        <ul>
+            <!-- v-for문을 돌려서 값을 보여준다. v-bind:key는 v-for문을 돌릴때 키를 지정하는것 -->
+            <li class="shadow" v-for="(todoItem, index) in todoItems" v-bind:key="todoItem">
+                {{ todoItem }}
+                <!-- 삭제버튼에 onclick이벤트로 removeTodo()를 호출 -->
+                <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                    <i class="fas fa-trash-alt"></i>
+                </span>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-
+    data: function(){
+        return {
+            todoItems: []
+        }
+    },
+    // 사용자정의 function들은 methods 안에 정의한다.
+    methods: {
+        removeTodo: function(todoItem, index){
+            // 삭제 메소드: 로컬스토리지에서 해당 값을 삭제하고 목록에서 값 삭제
+            console.log('remove items');
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1); // 배열에서 몇번째 인덱스로부터 몇개 지운다.
+        }
+    },
+    // created : vue instance가 생성되자마자 호출됨 (vue lifecycle hook)
+    created: function(){
+        if(localStorage.length > 0){
+            for(var i=0; i<localStorage.length; i++){
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+                    this.todoItems.push(localStorage.key(i));
+                }
+            }
+        }
+    }
 }
 </script>
 
 <style>
-
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.5rem;
+    background: white;
+    border-radius: 5px;
+}
+.removeBtn{
+    margin-left: auto;
+    color: #de4343;
+}
+.checkBtn{
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+}
+.checkBtnCompleted{
+    color: #b3adad;
+}
+.textCompleted{
+    text-decoration: line-through;
+    color: #b3adad;
+}
 </style>
