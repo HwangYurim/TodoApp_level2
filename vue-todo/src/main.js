e44c3e5de40450cc98df0ac451fb0e8db14902dd
sweetginger <hyr2902@gmail.com>
2019-06-27 06:20:50 +0900

    vuex 모듈화 방법 2

--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 // import 할때 변수로 임포트할 경우 {} 안에 넣어준다
 import { store } from './store/store'
 
-var app = new Vue({
+const app = new Vue({
   el: '#app',
   store,
   render: h => h(App)
